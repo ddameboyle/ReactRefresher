6319abbbd94d94005ed7bbf22e0299d377397304
David <ddameboyle@gmail.com>
2016-04-23 17:59:32 -0700

    Comments render

---
+++ app/index.jsx
@@ -0,0 +1,69 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var CommentBox = React.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+			Hello, world! I am a CommentBox.
+			<h1>Comments</h1>
+			<CommentList data={this.props.data}/>
+			<CommentForm/>
+			</div>
+			);
+	}
+});
+
+var CommentList = React.createClass({
+	render: function(){
+		var commentNodes = this.props.data.map(function (comment){
+			return(
+				<Comment author = {comment.author} key = {comment.id}>
+				{comment.text}
+				</Comment>
+				);
+		});
+		return (
+			<div className="commentList">
+			{commentNodes}
+			</div>
+			);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function(){
+		return (
+			<div className="commentForm">
+			COMMENT FORM
+			</div>
+			);
+	}
+});
+
+var Comment = React.createClass({
+	rawMarkup: function() {
+		var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+		return { __html: rawMarkup };
+	},
+	render: function(){
+		return(
+			<div className="comment">
+			<h2 className="commentAuthor">
+			{this.props.author}
+			</h2>
+			<span dangerouslySetInnerHTML = {this.rawMarkup()} />
+			</div>
+			);
+	}
+});
+
+var data = [
+{id: 1, author: "Pete Hunt", text:"This is one comment"},
+{id: 2, author: "Jordan Walke", text:"This is *another* comment"}
+];
+
+ReactDOM.render(
+	<CommentBox data = {data}/>,
+	document.getElementById('app')
+	);
